31548bcfd50a815063863e3160be61f4605e6cdc
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2019-05-29 18:51:33 +0700

    feat: install scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,85 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'react-hot-loader/patch',
-    path.join(__dirname, 'src/index.tsx')
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[hash].js',
-    publicPath: '/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(__dirname, 'index.hbs')
-    }),
-      new MiniCssExtractPlugin({
-          // Options similar to the same options in webpackOptions.output
-          // both options are optional
-          filename: '[name].css',
-          chunkFilename: '[id].css',
-      }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json', 'css'],
-    modules: [
-      'node_modules',
-      'src',
-    ]
-  },
-  devServer: {
-    hot: true,
-    inline: true,
-  },
-  module: {
-    rules: [
-        {
-          test: /\.tsx?$/,
-          use: [{loader: 'react-hot-loader/webpack'}, {loader: 'awesome-typescript-loader'}],
-          exclude: /node_modules/,
-        },
-        {
-            test: /\.css$/i,
-            use: [{
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                    // you can specify a publicPath here
-                    // by default it uses publicPath in webpackOptions.output
-                    // publicPath: '../',
-                },
-            }, {loader: 'css-loader'}],
-        },
-        {
-            test: /\.(woff|woff2)$/i,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
+    devtool: 'eval',
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.tsx')
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'index.hbs')
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.scss'],
+        modules: [
+            'node_modules',
+            'src',
+        ]
+    },
+    devServer: {
+        hot: true,
+        inline: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{loader: 'react-hot-loader/webpack'}, {loader: 'awesome-typescript-loader'}],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            // publicPath: '../',
+                        },
+                    }, {loader: 'css-loader'},
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: process.env.IS_DEVELOPMENT
+                        }
                     }
-                }
-            ]
-        }
-    ]
-  },
+                ],
+            },
+            {
+                test: /\.(woff|woff2)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     optimization: {
         splitChunks: {
             chunks: 'async',
